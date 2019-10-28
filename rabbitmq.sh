cc81cca445754e1c2bf21bf924e7501bb6c99bb2
Doncho Minkov <DonchoMinkov@gmail.com>
2019-10-28 14:45:04 +0200

    Added RabbitMQ

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -3,3 +3,6 @@ wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-ke
 echo "deb https://dl.bintray.com/rabbitmq/debian $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
 sudo apt update
 sudo apt -y install rabbitmq-server
+
+sudo systemctl enable rabbitmq-server
+sudo rabbitmq-plugins enable rabbitmq_management
