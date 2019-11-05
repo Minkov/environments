4ca9eb64d9dd24d689c7de501f7e2c391e8b479c
Doncho Minkov <DonchoMinkov@gmail.com>
2019-11-05 15:25:06 +0200

    Now working for all envs

--- apps-server/bootstrap.sh
+++ apps-server/bootstrap.sh
@@ -2,14 +2,17 @@
 
 # Initial
 
-sudo yum -y update
-
-sudo yum -y install vim git yum-utils device-mapper-persistent-data lvm2
-
 source /vagrant/.env
+
+echo ' --- Updating packages ---'
+sudo yum -y update > /dev/null
+
+echo ' --- Installing vim git yum-utils device-mapper-persistent-data lvm2 ---'
+sudo yum -y install vim git yum-utils device-mapper-persistent-data lvm2 > /dev/null
 
 # Docker
 
+echo ' --- Remove old docker packages if existing ---'
 sudo yum remove docker \
                 docker-client \
                 docker-client-latest \
@@ -17,35 +20,32 @@ sudo yum remove docker \
                 docker-latest \
                 docker-latest-logrotate \
                 docker-logrotate \
-                docker-engine
+                docker-engine > /dev/null
 
+echo '--- Adding docker repo ---'
 sudo yum-config-manager \
     --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+    https://download.docker.com/linux/centos/docker-ce.repo > /dev/null
 
-sudo yum install -y docker-ce docker-ce-cli containerd.io
+echo ' --- Installing docker ---'
+sudo yum install -y docker-ce docker-ce-cli containerd.io > /dev/null
 
 sudo usermod -aG docker vagrant
 
 sudo systemctl start docker
 
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin
 
 # Docker Compose
 
+if ! [ -x "$(command -v docker-compose)" ]; then
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# Clone repos
-
-cd /vagrant
-git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/SoftUni-Internal/interactive-deployment.git
-cd /vagrant/interactive-deployment
-git pull
+fi
 
 # Fireup docker
 
-cd /vagrant/interactive-deployment
-docker-compose -f docker-compose.prod.yml up -d
+cd /vagrant/apps
+docker-compose -f docker-compose.${ENVIRONMENT}.yml up -d
 
